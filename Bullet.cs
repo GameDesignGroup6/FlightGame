6098f671958a982f2e8106f9333d89e8a322137d
las203 <las203@case.edu>
2014-04-08 15:39:59 -0400

    Shooting stuff for the plane

---
+++ Bullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float bulletSpeed;
+
+	void Start() {
+		bulletSpeed = 50;
+	}
+
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * bulletSpeed, Space.World);
+		StartCoroutine ("Wait");
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds (10f);
+		Destroy (gameObject);
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.transform.tag == "Plane") {
+			Debug.Log ("Plane hit!");
+		}
+		Destroy (gameObject);
+	}
+}
---
+++ Shoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Fire ();
+		}
+	}
+
+	void Fire() {
+		GameObject clone = (GameObject) Instantiate (bullet, transform.position, transform.rotation);
+	}
+}
