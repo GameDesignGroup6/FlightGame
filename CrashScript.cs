4dcab67071cd3531c1272d169c5973eafdc42f9f
abc33kr <abc33kr@gmail.com>
2014-04-09 23:36:00 -0400

    weird, my github didn't clone this repo right.

---
+++ CrashScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrashScript : MonoBehaviour {
+
+	//these variables can be turned into private once the value is finalized
+	public float groundLevel;
+	public float crashVelocityThreshold;
+	public Transform crashedAirPlane;
+	// Use this for initialization
+	void Start () {
+	
+
+		groundLevel = 1.0;
+		crashVelocityThreshold = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void crash(){
+		if ((GetYPosition() <groundLevel)&&(GetAirSpeed>crashVelocityThreshold)){
+			Instantiate(crashedAirPlane, gameObject.rigidbody.position, gameObject.rigidbody.rotation);
+			Destroy(gameObject);
+			Debug.Log ("Plane " + gameObject.name + " crashed");
+		}
+	}
+
+	/**
+	 *This method uses speed in m/s not knots
+	 */
+	public float GetAirSpeed(){
+		return gameObject.rigidbody.velocity.magnitude;
+	}
+	public float GetYPosition(){
+		return gameObject.rigidbody.position.y;
+	}
+}
